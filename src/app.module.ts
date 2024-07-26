cb2aac3d549b4c8ee2d969aa09acc6c2e058b4bb
Geana Almeida <geana.almeida2000@gmail.com>
2024-07-26 11:19:19 -0300

    Configuração padrao, e conectado ao banco de dados

---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'db_lojagames',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
